3c00967ed29e8b26f7ac950c8f31ce0d3300eb59
gayathri140205 <gayathri140205@gmail>
2024-03-05 16:26:40 +0530

    books

---
+++ src/Footer.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="py-5 bg-dark"  style={{marginTop:"40%"}}>  
+      <div className="container text-center">
+     
+       <br/>
+        <p className="m-0 text-center fst-normal text-white">
+          Copyright @ 2024
+          <a
+            href="#"
+            style={{
+              textDecoration: "none",
+              color: " #32CD32",
+              fontWeight: "bold",
+            }}
+          >
+          
+          </a>{" "}
+      
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div className="container" >
+      <div className="row ">
+        <div className="heading text-white p-5 mt-3 mb-3 rounded-4 ">
+          <h1 className="text-center mt-3 "><span style={{color:"#f5f5dc"}}><i> Welcome to Gayathri Library Mangement System</i></span> </h1>
+        </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
